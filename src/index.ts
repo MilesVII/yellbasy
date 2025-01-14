26d0b31d9f62339ab0cdde1bfdb5240de93668b9
Miles Seventh <milesseventh@gmail.com>
2025-01-14 15:54:00 +0400

    add sth i dunno im tired

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,47 @@
-import { track } from "./track"
+import { download, track } from "./track"
 
 document.querySelector("button")?.addEventListener("click", play);
 
+const dynamics = [
+	[0, .1, .2],
+	[.1, 1, .02],
+	[1, .2, .04],
+	[.2, 0, 4]
+] as [number, number, number][];
+
 function play() {
+	const length = 4;
+
 	const tau = Math.PI * 2;
 	const noise = (a: number) =>
-		track(() => (Math.random() - .5) * a);
+		track(length, () => (Math.random() - .5) * a);
 	const sine = (f: number, o: number = 0, a: number = 1) =>
-		track(at => Math.sin(at * tau * f + o) * a);
+		track(length, at => Math.sin(at * tau * f + o) * a);
 	const saw = (f: number, a: number = 1) =>
-		track(at => ((at * f) % 1) * a).remap(-1, 1);
+		track(length, at => ((at * f) % 1) * a).remap(-1, 1);
 
-	const railroad = track().add(
+	const railroad = track(length).add(
 		noise(.005),
-		saw(80, .2)
-			.mul(sine(160))
-			.mul(sine(200, .5))
-			.normalize()
-		// sine()
+		// saw(80, .2)
+		// 	.mul(sine(160))
+		// 	.mul(sine(200, .5))
+		// 	.normalize(),
+		track(length)
+			.derpFill(dynamics)
+			.mul(.9)
+			.mul(sine(470)),
+		track(length)
+			.derpFill(dynamics)
+			.mul(.6)
+			.mul(sine(490)),
+		track(length)
+			.derpFill(dynamics)
+			.mul(.3)
+			.mul(sine(420)),
+		// sine(240),
+		// sine(240, .25)
 	);
+	// download(railroad.source())
 	railroad.source().start();
+	
 }
--- src/track.ts
+++ src/track.ts
@@ -1,4 +1,4 @@
-
+import toWav from "audiobuffer-to-wav";
 
 // export type Track = ReturnType<typeof track>;
 export type Feel = (at: number) => number;
@@ -6,10 +6,9 @@ export type Buffer = number[];
 type Factor = number | Track;
 
 const sampleRate = 69420;
-const l = 2;
 
-function buffer(feel?: Feel) {
-	const r = Array.from<number>({length: sampleRate * l});
+function buffer(length: number, feel?: Feel) {
+	const r = Array.from<number>({length: sampleRate * length});
 	if (feel)
 		r.forEach((_, i) => r[i] = feel(i / sampleRate));
 	return r;
@@ -50,9 +49,51 @@ function bufferRemap(buffer: Buffer, low: number, high: number): Buffer {
 	return buffer.map(s => remap(s, l, h, low, high));
 }
 
+function lerpFill(length: number, src: Buffer) {
+	const r: Buffer = [];
+	for (let i = 0; i < length; ++i) {
+		let at = i / length;
+		
+		if (at >= 1) {
+			console.error(`invalid at: ${at}`)
+			at = .999
+		};
+		const lix = src.length - 1;
+		const ix = Math.floor(at * src.length) - 1;
+		const nix = ix === lix ? 0 : (ix + 1);
+		r.push(remap(at, ix / lix, nix / lix, src[ix], src[nix]));
+	}
+	return track(length, r);
+}
+
+type Segment = [from: number, to: number, weight: number];
+function derpFill(length: number, segs: Segment[]) {
+	const allWeights = segs.reduce((p, [_f, _t, w]) => p + w, 0);
+	
+	return track(length, (at) => {
+		const globalAt = remap(at, 0, length, 0, allWeights);
+		let progress = 0;
+
+		for (const [f, t, w] of segs) {
+			if (globalAt < progress + w)
+				return remap(globalAt, progress, progress + w, f, t);
+			progress += w;
+		}
+		console.log(globalAt, at)
+		console.error("derpFill is faulty");
+		return 0;
+	});
+}
+
+export function download(source: AudioBufferSourceNode) {
+	const blob = new Blob([toWav(source.buffer!)], { type: 'audio/wav' });
+	window.open(URL.createObjectURL(blob));
+}
+
 function source(data: Buffer) {
 	const audioContext = new window.AudioContext({ sampleRate });
-	const audioBuffer = audioContext.createBuffer(1, sampleRate * l, sampleRate);
+	console.log(data.length, sampleRate)
+	const audioBuffer = audioContext.createBuffer(1, data.length, sampleRate);
 
 	const channelData = audioBuffer.getChannelData(0);
 	for (let i = 0; i < channelData.length; i++)
@@ -71,24 +112,33 @@ export type Track = {
 	add: (...additions: Track[]) => Track,
 	normalize: () => Track,
 	source: () => AudioBufferSourceNode,
-	remap: (low: number, high: number) => Track
+	remap: (low: number, high: number) => Track,
+	lerpFill: (src: Buffer) => Track,
+	derpFill: (segs: Segment[]) => Track
 } 
 
-export function track(src: Feel | Buffer = () => 0): Track {
-	const buf = Array.isArray(src) ? src : buffer(src)
+const numb = () => 0;
+export function track(length: number, src: Feel | Buffer = numb): Track {
+	const buf =
+		Array.isArray(src)
+			? src
+			: buffer(length, src);
 	return {
 		buffer: buf,
-		mul: (factor) => track(multiply(buf, factor)),
-		add: (...additions) => track(combine([buf, ...additions])),
-		normalize: () => track(normalize(buf)),
+		mul: (factor) => track(length, multiply(buf, factor)),
+		add: (...additions) => track(length, combine([buf, ...additions])),
+		normalize: () => track(length, normalize(buf)),
 		source: () => source(buf),
-		remap: (low, high) => track(bufferRemap(buf, low, high))
+		remap: (low, high) => track(length, bufferRemap(buf, low, high)),
+		lerpFill: (src) => lerpFill(length, src),
+		derpFill: (segs: Segment[]) => derpFill(length, segs)
 	};
 }
 
 export function remap(x: number, fromMin: number, fromMax: number, toMin: number, toMax: number) {
 	return (x - fromMin) / (fromMax - fromMin) * (toMax - toMin) + toMin;
 }
-// function lerp(k, from, to) {
+
+// function lerp(k: number, from: number, to: number) {
 // 	return from + (to - from) * k;
 // }
